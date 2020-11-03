61d634bebaa064d7fe581912854aea5caf52a1a2
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-11-02 20:26:54 -0800

    added event listeners to ul items

--- js/scripts.js
+++ js/scripts.js
@@ -52,12 +52,19 @@ function displayContactDetails(addressBookToDisplay) {
   let contactsList = $("ul#contacts");
   let htmlForContactInfo = "";
   addressBookToDisplay.contacts.forEach(function(contact){
-    htmlForContactInfo += "<li id=" + contact.id + ">" + contact/firstName + " " + contact.lastName +"</li>";
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName +"</li>";
   });
   contactsList.html(htmlForContactInfo);
 }
 
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function(){
+    console.log("The id of this <li> is" + this.id + ".");
+  });
+};
+
 $(document).ready(function(){
+  attachContactListeners();
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
     const inputtedFirstName = $("input#new-first-name").val();
@@ -65,6 +72,6 @@ $(document).ready(function(){
     const inputtedPhoneNumber = $("input#new-phone-number").val();
     let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
     addressBook.addContact(newContact);
-    console.log(addressBook.contacts);
+    displayContactDetails(addressBook);
   });
 });
